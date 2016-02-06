83dd396dafa7992f3906665392ee3fd078280e12
Masakazu Emi <masakazu.emi@gmail.com>
2016-02-06 23:50:08 +0000

    add submodule init

--- dotfilelink.sh
+++ dotfilelink.sh
@@ -1,5 +1,13 @@
 #! /bin/bash
+
+cd ~/dotfiles/
+git submodule init
+git submodule update
+cd ~/
+
 ln -s ~/dotfiles/_vimrc ~/.vimrc
 ln -s ~/dotfiles/_gvimrc ~/.gvimrc
 ln -s ~/dotfiles/_vim ~/.vim
+
+mkdir tmp
 
