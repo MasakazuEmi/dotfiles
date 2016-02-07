5f2e75f9f5cad1e7248a5bd7466e71c2830c8b9f
Masakazu Emi <masakazu.emi@gmail.com>
2016-02-08 01:26:50 +0900

    rm link

--- dotfilelink.sh
+++ dotfilelink.sh
@@ -5,11 +5,15 @@ git submodule init
 git submodule update
 cd ~/
 
+rm ~/.vimrc
 ln -s ~/dotfiles/_vimrc ~/.vimrc
+rm ~/.gvimrc
 ln -s ~/dotfiles/_gvimrc ~/.gvimrc
+rm ~/.vim
 ln -s ~/dotfiles/_vim ~/.vim
 
+rm ~/.gitconfig
 ln -s ~/dotfiles/_gitconfig .gitconfig
 
-mkdir tmp
+mkdir -p tmp 2>/dev/null
 
